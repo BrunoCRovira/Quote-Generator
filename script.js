5282a53ac378dc4a356605e9610e6da67e1a81f1
BrunoCRovira <102754581+BrunoCRovira@users.noreply.github.com>
2023-09-11 04:25:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ const quoteText = document.getElementById('quote');
 const quoteAhutor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 let apiQuotes = [];
+//Show Loading 
+function loading(){
+    loader.hidden =false;
+    quoteContainer.hidden=true;
+}
 
+//Hide LOading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
 
 //show New Quote
 
 function newQuote(){
     // Pick a random quote from apiQuotes array
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     quoteAhutor.textContent = quote.author; 
     // Check Quote Lengh to determing Styling
@@ -19,12 +29,15 @@ function newQuote(){
     } else { 
        quoteText.classList.remove('long-quote')
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+     complete();
 }
 
 // Get Quotes From API
 
 async function getQUotes(){
+     loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
